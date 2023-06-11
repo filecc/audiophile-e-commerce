59489810c1ddf3f5be1448a8d4ffe93ef71ec3ce
filecc <fil.gia@live.it>
2023-06-11 08:58:41 +0200

    fixed order on lg:

--- src/app/components/products-show/SingleProduct.jsx
+++ src/app/components/products-show/SingleProduct.jsx
@@ -7,9 +7,11 @@ export default function SingleProduct({item})
     
     return (
         <div className="flex flex-col lg:flex-row items-center justify-center lg:justify-between gap-5 px-4 my-10">
-            <div className={"relative h-[350px] lg:h-[560px] overflow-hidden rounded w-full lg:w-6/12 " + (item.id % 2 && 'order-2')}>
+            <div className={"relative h-[350px] overflow-hidden rounded w-full lg:hidden"}>
                 <Image className="p-4 rounded md:hidden object-contain" src={item.categoryImage.mobile} alt={item.name} fill={true} />
                 <Image className="p-4 rounded hidden md:block lg:hidden object-cover" src={item.categoryImage.tablet} alt={item.name} fill={true} />
+            </div>
+            <div className={"relative lg:h-[560px] overflow-hidden rounded hidden lg:block lg:w-6/12 " + (item.id % 2 && 'order-2')}>
                 <Image className="p-4 rounded hidden lg:inline-block object-contain" src={item.categoryImage.desktop} alt={item.name} fill={true} />
             </div>
             <div className="flex flex-col items-center lg:items-start justify-center gap-5 px-4 text-center lg:text-start lg:w-6/12">
